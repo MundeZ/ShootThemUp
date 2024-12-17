7fd0c637d0828f487ba5495c799a29bcb5e2be63
Roma <meynewo@gmail.com>
2024-12-17 22:01:43 +0700

    add crosshair

--- Source/ShootThemUp/Private/Components/STUWeaponComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUWeaponComponent.cpp
@@ -47,6 +47,16 @@ void USTUWeaponComponent::Reload()
     ChangeClip();
 }
 
+bool USTUWeaponComponent::GetWeaponUIData(FWeaponUIData& UIData) const
+{
+    if (CurrentWeapon)
+    {
+        UIData = CurrentWeapon->GetUIData();
+        return true;
+    }
+    return false;
+}
+
 void USTUWeaponComponent::BeginPlay()
 {
     Super::BeginPlay();
--- Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -23,6 +23,8 @@ public:
     void ChangeClip();
     bool CanReload() const;
 
+    FWeaponUIData GetUIData() const { return UIData; };
+
 protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
     USkeletalMeshComponent* WeaponMesh;
@@ -35,6 +37,9 @@ protected:
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
     FAmmoData DefaultAmmo{15, 10, false};
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "UI")
+    FWeaponUIData UIData;
 
     virtual void BeginPlay() override;
     virtual void MakeShot();
