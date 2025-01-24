bdd1d9623df453f2d51111a47d4ae61ccd8aa1c6
Roma <meynewo@gmail.com>
2025-01-24 08:42:01 +0700

    add rotation yaw for pickup

--- Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
+++ Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
@@ -22,6 +22,7 @@ void ASTUBasePickup::BeginPlay()
 {
     Super::BeginPlay();
     check(CollisionComponent);
+    GenerateRotationYaw();
 }
 
 void ASTUBasePickup::NotifyActorBeginOverlap(AActor* OtherActor)
@@ -39,6 +40,7 @@ void ASTUBasePickup::NotifyActorBeginOverlap(AActor* OtherActor)
 void ASTUBasePickup::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+    AddActorLocalRotation(FRotator(0.0f, RotationYaw, 0.0f));
 }
 
 bool ASTUBasePickup::GivePickupTo(APawn* PlayerPawn)
@@ -60,9 +62,16 @@ void ASTUBasePickup::PickupWasTaken()
 
 void ASTUBasePickup::RespawnPickup()
 {
+    GenerateRotationYaw();
     CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
     if (GetRootComponent())
     {
         GetRootComponent()->SetVisibility(true, true);
     }
+}
+
+void ASTUBasePickup::GenerateRotationYaw()
+{
+    const auto Direction = FMath::RandBool() ? 1.0f : -1.0f;
+    RotationYaw = FMath::RandRange(1.0f, 2.0f) * Direction;
 }
--- Source/ShootThemUp/Public/Pickups/STUBasePickup.h
+++ Source/ShootThemUp/Public/Pickups/STUBasePickup.h
@@ -30,7 +30,9 @@ public:
     virtual void Tick(float DeltaTime) override;
 
 private:
+    float RotationYaw = 0.0f;
     virtual bool GivePickupTo(APawn* PlayerPawn);
     void PickupWasTaken();
     void RespawnPickup();
+    void GenerateRotationYaw();
 };
