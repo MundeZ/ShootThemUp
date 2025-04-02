c883312dc65f46d97d86b531d8053f0f109346fe
Roma <meynewo@gmail.com>
2025-04-02 21:49:36 +0700

    add spawn bots

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -1,7 +1,8 @@
 // Shoot Them Up Game, All Rights
 
 #include "STUGameModeBase.h"
-#include "Player/STUBaseCharacter.h"
+
+#include "AIController.h"
 #include "Player/STUPlayerController.h"
 #include "UI/STUGameHUD.h"
 
@@ -10,4 +11,33 @@ ASTUGameModeBase::ASTUGameModeBase()
     DefaultPawnClass = ASTUGameModeBase::StaticClass();
     PlayerControllerClass = ASTUPlayerController::StaticClass();
     HUDClass = ASTUGameHUD::StaticClass();
+}
+
+void ASTUGameModeBase::StartPlay()
+{
+    Super::StartPlay();
+
+    SpawnBots();
+}
+
+UClass* ASTUGameModeBase::GetDefaultPawnClassForController_Implementation(AController* InController)
+{
+    if (InController && InController->IsA<AAIController>())
+    {
+        return AIPawnClass;
+    }
+    return Super::GetDefaultPawnClassForController_Implementation(InController);
+}
+
+void ASTUGameModeBase::SpawnBots()
+{
+    if (!GetWorld()) return;
+
+    for (int32 i = 0; i < GameData.PlayersNum - 1; ++i)
+    {
+        FActorSpawnParameters SpawnInfo;
+        SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+        const auto STUAIController = GetWorld()->SpawnActor<AAIController>(AIControllerClass, SpawnInfo);
+        RestartPlayer(STUAIController);
+    }
 }
